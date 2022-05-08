fae152995ec75292200812c9ff8d5fbdf9c0b6f6
Kotarou <jz.van94@gmail.com>
2022-05-08 12:54:51 +0800

    add unknown if no git found

--- cmake/GitVersion.cmake
+++ cmake/GitVersion.cmake
@@ -6,4 +6,6 @@ if (GIT_FOUND)
             OUTPUT_STRIP_TRAILING_WHITESPACE
             ERROR_QUIET)
     string(REGEX REPLACE "^heads\\/(.*)-0-(g.*)$" "\\1~\\2" GIT_VERSION "${GIT_VERSION}")
+else ()
+    set(GIT_VERSION unknown)
 endif ()
